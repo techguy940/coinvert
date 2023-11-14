6a501fed80959080725240c5d68ec74b79ce0190
techguy940 <51826754+techguy940@users.noreply.github.com>
2023-11-14 21:53:37 +0530

    Added favourites feature

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -8,6 +8,8 @@ import { sub, add } from "date-fns";
 import axios from "axios";
 import CanvasJSReact from '@canvasjs/react-charts';
 import recent from '../assets/recent.png';
+import heart from '../assets/heart.png';
+import heartFilled from '../assets/heart-filled.png';
 
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
@@ -39,7 +41,7 @@ const groupBadgeStyles = {
   const formatGroupLabel = (data) => (
     <div style={groupStyles}>
       <span>{data.label}</span>
-      <span style={data.label === "History" ? {} : groupBadgeStyles}>{data.label === "History" ? <img src={recent} width="15" height="15" /> : data.options.length}</span>
+      <span style={data.label === "History" || data.label === "Favourites" ? {} : groupBadgeStyles}>{data.label === "History" ? <img src={recent} width="15" height="15" /> : data.label === "Favourites" ? <img src={heartFilled} width="15" height="15" /> :data.options.length}</span>
     </div>
   );
 
@@ -54,13 +56,49 @@ function Convert(){
     const [toCurrency, setToCurrency] = useState("")
     const [dataPoints, setDataPoints] = useState([])
     const [graphReady, setGraphReady] = useState(false)
+    const [favouriteStatus, setFavouriteStatus] = useState(false)
     const [historyData, setHistoryData] = useState(null)
+    const [favouriteData, setFavouriteData] = useState(null)
     const [exchangeClass, setExchangeClass] = useState("convert-exchange")
+
+    const getFavouriteStatus = (cur1, cur2) => {
+        if (localStorage.getItem("favourites") === null){
+            return false
+        }
+        const favourites = JSON.parse(localStorage.getItem("favourites"))
+        const isFav = favourites.filter(cur => cur.from === cur1 && cur.to === cur2).length;
+        return isFav === 1
+    }
+
+    const addFavourite = () => {
+        if (favouriteStatus === true){
+            const favourites = JSON.parse(localStorage.getItem("favourites"))
+            favourites.forEach(cur => {
+                if (cur.from === currency1.value && cur.to === currency2.value){
+                    favourites.splice(favourites.indexOf(cur), 1)
+                    console.log(favourites)
+                    localStorage.setItem("favourites", JSON.stringify(favourites))
+                    setFavouriteStatus(false)
+                    return
+                }
+            })
+        } else {
+            if (localStorage.getItem("favourites") === null){
+                localStorage.setItem("favourites", JSON.stringify([]))
+            }
+
+            const favourites = JSON.parse(localStorage.getItem("favourites"))
+            favourites.push({"from": currency1.value, "to": currency2.value, "value": `${currency1.value}/${currency2.value}`, "text": `${currency1.value}/${currency2.value}`, "currencyName": `${currency1.value}/${currency2.value}`})
+            localStorage.setItem("favourites", JSON.stringify(favourites))
+            setFavouriteStatus(true)
+        }
+    }
     
     const handleChange1 = (e) => {
         if ("from" in e){
             setCurrency1(currencies.filter(curr => curr.value === e.from)[0])
             setCurrency2(currencies.filter(curr => curr.value === e.to)[0])
+            setFavouriteStatus(getFavouriteStatus(e.from, e.to))
             return
         }
         setCurrency1(e)
@@ -70,6 +108,7 @@ function Convert(){
         if ("from" in e){
             setCurrency1(currencies.filter(curr => curr.value === e.from)[0])
             setCurrency2(currencies.filter(curr => curr.value === e.to)[0])
+            setFavouriteStatus(getFavouriteStatus(e.from, e.to))
             return
         }
         setCurrency2(e)
@@ -88,10 +127,11 @@ function Convert(){
         const temp = currency1;
         setCurrency2(currency1);
         setCurrency1(currency2);
+        setFavouriteStatus(getFavouriteStatus(currency1.value, currency2.value))
 
         const temp2 = fromCurrency;
         setFromCurrency(toCurrency);
-        setToCurrency(temp);
+        setToCurrency(temp2);
     }
 
     useEffect(() => {
@@ -111,7 +151,6 @@ function Convert(){
         if (currency1 === null || currency2 === null){
             return
         }
-        console.log(currency1, currency2)
 
         if (localStorage.getItem("history") === null){
             localStorage.setItem("history", JSON.stringify([]))
@@ -132,42 +171,65 @@ function Convert(){
         localStorage.setItem("history", JSON.stringify(historyData))
     }, [currency1, currency2])
 
-    // useEffect(() => {
-    //     if (currency1 === null || currency2 === null){
-    //         return
-    //     }
-    //     // get daily historic data (1 month)
-    //     const now = new Date()
-    //     var past = sub(now, {months: 1})
-    //     const dataPoints_ = []
-    //     axios.get(HISTORIC_BASE, {
-    //         params: {
-    //             api_key: HISTORIC_API_KEY,
-    //             start_date: past.toISOString().substring(0, 10),
-    //             end_date: now.toISOString().substring(0, 10),
-    //             base: currency1.value,
-    //             currencies: currency2.value
-    //         }
-    //     }).then(res => {
-    //         var count = 0;
-    //         const keys = Object.keys(res.data.rates)
-    //         keys.forEach(key => {
-    //             dataPoints_.push({x: new Date(key), y: res.data.rates[key][currency2.value]})
-    //             count = count + 1;
-    //             if (count === keys.length) {
-    //                 setDataPoints(dataPoints_)
-    //                 console.log(dataPoints_)
-    //                 setGraphReady(true)
-    //             }
-    //         })
-    //     }).catch(err => console.log(err))
-    // }, [currency1, currency2])
+    useEffect(() => {
+        if (currency1 === null || currency2 === null){
+            return
+        }
+
+        if (localStorage.getItem("favourites") === null){
+            localStorage.setItem("favourites", JSON.stringify([]))
+        }
+
+        setFavouriteStatus(getFavouriteStatus(currency1.value, currency2.value))
+    }, [currency1, currency2])
+
+    useEffect(() => {
+        if (localStorage.getItem("favourites") === null){
+            localStorage.setItem("favourites", JSON.stringify([]))
+        }
+        setFavouriteData(JSON.parse(localStorage.getItem("favourites")))
+    }, [favouriteStatus])
+
+    useEffect(() => {
+        if (currency1 === null || currency2 === null){
+            return
+        }
+        // get daily historic data (1 month)
+        const now = new Date()
+        var past = sub(now, {months: 1})
+        const dataPoints_ = []
+        axios.get(HISTORIC_BASE, {
+            params: {
+                api_key: HISTORIC_API_KEY,
+                start_date: past.toISOString().substring(0, 10),
+                end_date: now.toISOString().substring(0, 10),
+                base: currency1.value,
+                currencies: currency2.value
+            }
+        }).then(res => {
+            var count = 0;
+            const keys = Object.keys(res.data.rates)
+            keys.forEach(key => {
+                dataPoints_.push({x: new Date(key), y: res.data.rates[key][currency2.value]})
+                count = count + 1;
+                if (count === keys.length) {
+                    setDataPoints(dataPoints_)
+                    console.log(dataPoints_)
+                    setGraphReady(true)
+                }
+            })
+        }).catch(err => console.log(err))
+    }, [currency1, currency2])
 
     useEffect(() => {
         if (localStorage.getItem("history") === null){
             localStorage.setItem("history", JSON.stringify([]))
         }
+        if (localStorage.getItem("favourites") === null){
+            localStorage.setItem("favourites", JSON.stringify([]))
+        }
         setHistoryData(JSON.parse(localStorage.getItem("history")))
+        setFavouriteData(JSON.parse(localStorage.getItem("favourites")))
     }, [])
 
     return (
@@ -182,7 +244,7 @@ function Convert(){
                     <div className="convert-input">
                         <span className="convert-origin">From</span>
                         <Select 
-                            options={[{label: "History", options: historyData}, {label: "Currencies", options: currencies}]}
+                            options={[{label: "History", options: historyData}, {label: "Favourites", options: favouriteData}, {label: "Currencies", options: currencies}]}
                             formatGroupLabel={formatGroupLabel}
                             value={currency1}
                             onChange={handleChange1}
@@ -201,13 +263,18 @@ function Convert(){
                             )}
                         />
                     </div>
-                    <div className={exchangeClass} onClick={animateAndExchangeCurrencies}>
-                        <img src={exchange} />
+                    <div className="convert-mobile">
+                        <div className={exchangeClass} onClick={animateAndExchangeCurrencies}>
+                            <img src={exchange} />
+                        </div>
+                        <div className="convert-favourite" onClick={addFavourite}>
+                            <img src={favouriteStatus ? heartFilled : heart} width="40" height="40"/>
+                        </div>
                     </div>
                     <div className="convert-input">
                         <span className="convert-origin">To</span>
                         <Select 
-                            options={currencies}
+                            options={[{label: "History", options: historyData}, {label: "Favourites", options: favouriteData}, {label: "Currencies", options: currencies}]}
                             value={currency2}
                             onChange={handleChange2}
                             styles={{
@@ -225,14 +292,19 @@ function Convert(){
                             )}
                         />
                     </div>
+                    <div className="convert-favourite main" onClick={addFavourite}>
+                        <img src={favouriteStatus ? heartFilled : heart} width="30" height="30"/>
+                    </div>
                 </div>
                 <div className="convert-currency-inputs">
                     <div className="convert-currency-input">
                         <input type="number" min="0" placeholder={currency1?.value} value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)} disabled={currency1 === null || currency2 === null}/>
                     </div>
+                    <div></div>
                     <div className="convert-currency-input">
                         <input type="number" min="0" placeholder={currency2?.value} value={toCurrency} disabled />
                     </div>
+                    <div></div>
                 </div>
                 { graphReady && <div className="convert-graph">
                     <CanvasJSChart options={{
